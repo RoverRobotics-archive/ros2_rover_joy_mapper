d444ee57fff8d117227a16025df81a9f4370412d
padiln <nicholas.padilla@npcomplete.io>
2020-08-27 15:42:33 -0500

    fixed repeat halt command issue

--- scripts/modules/Topics.py
+++ scripts/modules/Topics.py
@@ -77,7 +77,7 @@ class TwistTopic:
         msg.angular.x = self._ang_throttle_coef * self._convert_input(self.roll, controller)
         msg.angular.y = self._ang_throttle_coef * self._convert_input(self.pitch, controller)
         msg.angular.z = self._ang_throttle_coef * self._convert_input(self.yaw, controller)
-        if msg == self._HALT and False: # TODO: This makes it so halts are always published
+        if msg == self._HALT:
             if self.publish_multiple_halts or not self._last_message_was_halt:
                 self._publisher.publish(msg)
             self._last_message_was_halt = True
